67348969a5b3ac98667e3e3b7404e3872a9eb5f7
AlexELEC <alexelecv5@gmail.com>
2021-03-27 08:46:02 +0200

    FregatUA: fix MAC

--- FregatUA.py
+++ FregatUA.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+from random import choice
 
 root_dir = os.path.dirname(sys.argv[0])
 sys.path.append(root_dir)
 
 import utils
 
+hex_characters = '0123456789ABCDEF'
+rnd_mac = [choice(hex_characters) for _ in range(4)]
+MAC = 'E4:27:71:FF:{0}{1}:{2}{3}'.format(rnd_mac[0], rnd_mac[1], rnd_mac[2], rnd_mac[3])
+
 class Scraper:
     def __init__(self):
         self.source = 'FregatUA'
         self.plist = 'https://raw.githubusercontent.com/AlexELEC/TVLINK-ADDONS/main/plist/fregat'
         self.link = 'ext:{0}:'.format(self.source)
-        self.headers = {'User-Agent': 'SmartSDK', 'x-smartlabs-mac-address': 'E4:27:71:FF:00:FF'}
+        self.headers = {'User-Agent': 'SmartLabs/1.51652.472 (sml723x, SML-482) SmartSDK/1.5.63-rt-25 Qt/4.7.3 API/20121210', 'x-smartlabs-mac-address': MAC}
 
     def getHeaders(self):
         return self.headers
