4d065fbd034f0231ca4eb259848f72a4e569f272
AlexELEC <alexelecv5@gmail.com>
2022-01-19 18:54:24 +0200

    FregatUA: update addon (get playlist from fe.ott.fregat.net)

--- FregatUA.py
+++ FregatUA.py
@@ -4,9 +4,17 @@ import os, sys
 from random import choice
 
 root_dir = os.path.dirname(sys.argv[0])
+libs_dir = os.path.join(root_dir, 'libs')
+ssv_file = os.path.join(libs_dir, 'soupsieve.whl')
+bs4_file = os.path.join(libs_dir, 'beautifulsoup.whl')
+
 sys.path.append(root_dir)
+sys.path.append(libs_dir)
+sys.path.insert(0, ssv_file)
+sys.path.insert(0, bs4_file)
 
 import utils
+from bs4 import BeautifulSoup
 
 hex_characters = '0123456789ABCDEF'
 rnd_mac = [choice(hex_characters) for _ in range(4)]
@@ -15,8 +23,8 @@ MAC = 'E4:27:71:FF:{0}{1}:{2}{3}'.format(rnd_mac[0], rnd_mac[1], rnd_mac[2], rnd
 class Scraper:
     def __init__(self):
         self.source = 'FregatUA'
-        self.plist = 'https://raw.githubusercontent.com/AlexELEC/TVLINK-ADDONS/main/plist/fregat'
-        self.link = 'ext:{0}:'.format(self.source)
+        self.plist = 'http://fe.ott.fregat.net/CacheClient/ncdxml/ChannelPackage/list_channels?channelPackageId=20813412&locationId=1000050&from=0&to=2147483647&lang=ru'
+        self.link = f'ext:{self.source}:'
         self.headers = {'User-Agent': 'SmartLabs/1.51652.472 (sml723x, SML-482) SmartSDK/1.5.63-rt-25 Qt/4.7.3 API/20121210', 'x-smartlabs-mac-address': MAC}
 
     def getHeaders(self):
@@ -26,18 +34,21 @@ class Scraper:
         LL=[]
         group = self.source
         http = utils.getURL(self.plist)
-        http = utils.clean_m3u(http)
-        L = http.splitlines()
+        soup = BeautifulSoup(http, "xml")
+        titles = soup.find_all('bcname')
+        links = soup.find_all('url')
+        logos = soup.find_all('logo')
 
-        for cnLine in L:
+        for i in range(0, len(titles)):
             try:
-                data, group, logo = utils.group_logo_m3u(cnLine)
-                tail = data.partition(',')[2]
-                if 'http' in tail:
-                    head,sep,tail = tail.partition('http')
-                    title = head.strip()
+                title = titles[i].get_text()
+                lnk = links[i].get_text()
+                img = logos[i].get_text()
+                if title and 'http' in lnk:
                     ids = utils.title_to_crc32(title)
-                    url = self.link + (sep+tail).strip()
+                    url = f'{self.link}{lnk}'
+                    if img: logo = f"http://212.115.255.109/images/c90x90/{img}"
+                    else: logo = ''
                     LL.append((ids, title, group, url, logo))
                 else: continue  
             except: pass
