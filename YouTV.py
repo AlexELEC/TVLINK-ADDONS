3a8cc6e5d28020d2942865b1fde7c33a6c439e6b
AlexELEC <alexelecv5@gmail.com>
2022-04-30 17:33:31 +0300

    YouTV: fix token cache

--- YouTV.py
+++ YouTV.py
@@ -7,14 +7,13 @@ from base64 import b64decode
 root_dir = os.path.dirname(sys.argv[0])
 cache_dir = os.path.join(root_dir, 'cache')
 sys.path.append(root_dir)
+import utils
 
 EMAIL = "mail@email.com"
 PASSW = "password"
 
 # only free channels: True or False
 ONLY_FREE = True
-
-import utils
 
 if not os.path.isdir(cache_dir): os.makedirs(cache_dir)
 
@@ -29,57 +28,63 @@ def getDevID():
             f.write(f"{dev_id}")
     return str(dev_id)
 
+DEV_ID = getDevID()
+
+API_URL = 'https://api.youtv.com.ua'
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
+           'Accept': 'application/vnd.youtv.v8+json',
+           'Referer': 'https://youtv.ua/',
+           'Device-Uuid': DEV_ID}
+
+TOKEN_PATCH = os.path.join(cache_dir, "YouTV")
+
+def refreshToken():
+    data = {'email': EMAIL, 'password': PASSW}
+    req = requests.post(f"{API_URL}/auth/login", data=data, headers=HEADERS)
+    token = json.loads(req.text)["token"]
+    with open(TOKEN_PATCH, 'w') as f:
+        f.write(f"{token}")
+    return token
+
+def getToken():
+    if os.path.isfile(TOKEN_PATCH):
+        with open(TOKEN_PATCH, 'r') as f:
+            token = f.readline()
+        token.strip()
+        token_cut = token.split('.')
+        token_part = ''
+        for i in range( len(token_cut) - 1 ):
+            token_part += token_cut[i]
+        token_part = token_part + '=' * (4 - len(token_part) % 4) if len(token_part) % 4 != 0 else token_part
+        time_exp = b64decode(token_part).decode('utf-8')
+        time_exp = re.search('"exp":(.*?),"', time_exp, re.IGNORECASE).group(1)
+        if int( time.time() ) < int(time_exp) - 10000:
+            return token
+        else:
+            return refreshToken()
+    else:
+        return refreshToken()
+
+TOKEN = getToken()
+
+
 class Scraper:
     def __init__(self):
-        self.TOKEN = None
         self.source = 'YouTV'
-        self.token_patch = os.path.join(cache_dir, self.source)
         self.link = f'ext:{self.source}:'
-        self.api_url = 'https://api.youtv.com.ua'
-        self.dev_id = getDevID()
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0',
-                        'Accept': 'application/vnd.youtv.v8+json',
-                        'Referer': 'https://youtv.ua/',
-                        'Device-Uuid': self.dev_id}
+        self.headers = HEADERS
 
     def getHeaders(self):
         return self.headers
 
-    def refreshToken(self):
-        data = {'email': EMAIL, 'password': PASSW}
-        req = requests.post(f"{self.api_url}/auth/login", data=data, headers=self.headers)
-        token = json.loads(req.text)["token"]
-        with open(self.token_patch, 'w') as f:
-            f.write(f"{token}")
-        return token
-
-    def getToken(self):
-        if not self.TOKEN:
-            if os.path.isfile(self.token_patch):
-                with open(self.token_patch, 'r') as f:
-                    token = f.readline()
-                token.strip()
-                token_cut = token.split('.')
-                token_part = ''
-                for i in range( len(token_cut) - 1 ):
-                    token_part += token_cut[i]
-                token_part = token_part + '=' * (4 - len(token_part) % 4) if len(token_part) % 4 != 0 else token_part
-                time_exp = b64decode(token_part).decode('utf-8')
-                time_exp = re.search('"exp":(.*?),"', time_exp, re.IGNORECASE).group(1)
-                if int( time.time() ) < int(time_exp) - 10000:
-                    self.TOKEN = token
-                else:
-                    self.TOKEN = self.refreshToken()
-            else:
-                self.TOKEN = self.refreshToken()
-        return self.TOKEN
-
     def Channels(self):
         LL=[]
-        token = self.getToken()
+        global TOKEN
+        TOKEN = getToken()
         self.headers.update({'Content-Type': 'text/plain; charset=UTF-8'})
-        self.headers.update({'Authorization': f"Bearer {token}"})
-        http = requests.post(f"{self.api_url}/playlist", headers=self.headers)
+        self.headers.update({'Authorization': f"Bearer {TOKEN}"})
+        http = requests.post(f"{API_URL}/playlist", headers=self.headers)
         L = json.loads(http.text)["data"]
 
         for cnLine in L:
@@ -107,8 +112,7 @@ class Scraper:
             utils.ch_inputs_DB(self.source, LL)
 
     def getLink(self, lnk):
-        token = self.getToken()
-        self.headers.update({'Authorization': f"Bearer {token}"})
+        self.headers.update({'Authorization': f"Bearer {TOKEN}"})
         http = requests.get(lnk, headers=self.headers)
         url = 'https:' + json.loads(http.text)["p"]
         return url
