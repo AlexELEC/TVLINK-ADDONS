cc361935b67a920e761b5c5116475fde5708d4b7
AlexELEC <alexelecv5@gmail.com>
2022-06-10 21:25:49 +0300

    add addon Hochu

---
+++ Hochu.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+root_dir = os.path.dirname(sys.argv[0])
+libs_dir = os.path.join(root_dir, 'libs')
+ssv_file = os.path.join(libs_dir, 'soupsieve.whl')
+bs4_file = os.path.join(libs_dir, 'beautifulsoup.whl')
+
+sys.path.append(root_dir)
+sys.path.append(libs_dir)
+sys.path.insert(0, ssv_file)
+sys.path.insert(0, bs4_file)
+
+import utils
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self):
+        self.source = 'Hochu'
+        self.site = 'http://hochu.tv'
+        self.link = f'ext:{self.source}:'
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+                        'Referer': self.site}
+
+    def getHeaders(self):
+        return self.headers
+
+    def Channels(self):
+        LL = []
+        http = utils.getURL(self.site)
+        soup = BeautifulSoup(http, "html.parser")
+
+        for tag in soup.find_all('a', {'target': "_blank"}):
+            try:
+                title = tag.text.strip()
+                ids = utils.title_to_crc32(title)
+                url = self.link + self.site + tag.get('href')
+                img = self.site + tag.find('img').get('src')
+                LL.append((ids, title, "Эротика", url, img))
+            except: pass
+
+        if LL:
+            # Loading a Tuple into a Database (source, Tuple)
+            utils.ch_inputs_DB(self.source, LL)
+
+    def getLink(self, lnk):
+        http = utils.getURL(lnk)
+        soup = BeautifulSoup(http, "html.parser")
+        php_lnk = soup.find('iframe').get('src')
+        http = utils.getURL(php_lnk, headers=self.headers)
+        url = utils.mfind(http, 'file:"', '"});')
+        return url
