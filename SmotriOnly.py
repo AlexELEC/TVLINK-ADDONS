54f097cf35272c5f92d4ee08b3384349812af272
AlexELEC <alexelecv5@gmail.com>
2023-05-12 13:55:05 +0300

    SmotriOnly: fix url

--- SmotriOnly.py
+++ SmotriOnly.py
@@ -19,7 +19,7 @@ from bs4 import BeautifulSoup
 class Scraper:
     def __init__(self):
         self.source = 'SmotriOnly'
-        self.site = 'http://smotrite.only-tv.org/'
+        self.site = 'http://smotrite.only-tv.org'
         self.link = f'ext:{self.source}:'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109) Gecko/20100101 Firefox/112.0',
                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
