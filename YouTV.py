32cc8b20bf3e489d1a7df99e485504e8e8147f40
AlexELEC <alexelecv5@gmail.com>
2024-12-29 12:04:04 +0200

    YouTV: optimize addon

--- YouTV.py
+++ YouTV.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, json
-import requests, uuid
+import requests
+from pathlib import Path
 
 root_dir = os.path.dirname(sys.argv[0])
 cache_dir = os.path.join(root_dir, 'cache')
@@ -9,34 +10,38 @@ sys.path.append(root_dir)
 
 import utils
 
+SOURCE = Path(__file__).stem
+
 if not os.path.isdir(cache_dir): os.makedirs(cache_dir)
+devid_patch = os.path.join(cache_dir, f"{SOURCE}.dev")
+
+def getDevID():
+    if os.path.isfile(devid_patch):
+        with open(devid_patch, 'r') as f:
+            dev_id = f.readline()
+    else:
+        import uuid
+        dev_id = uuid.uuid4().int
+        with open(devid_patch, 'w') as f:
+            f.write(f"{dev_id}")
+    return str(dev_id)
+
+DEV_ID = getDevID()
+
 
 class Scraper:
     def __init__(self):
-        self.source = 'YouTV'
-        self.link = f'ext:{self.source}:'
+        self.link = f'ext:{SOURCE}:'
         self.api_url = 'https://api.youtv.com.ua'
-        self.dev_id = self.getDevID()
         self.headers = {'user-agent': 'youtv/3.23.13+8004 (Samsung MB2; Android; 6.0.1; Mobile; null; MHC19J.20170619.091635 test-keys; 1920x1008)',
                         'accept': 'application/vnd.youtv.v9+json',
-                        'device-uuid': self.dev_id,
+                        'device-uuid': DEV_ID,
                         'applicationid': 'ua.youtv.youtv',
                         'device': 'mobile',
                         'restriction': 'uhd:true;4k:true;8k:true;hdr:false;adult:true;children:false'}
 
     def getHeaders(self):
         return self.headers
-
-    def getDevID(self):
-        devid_patch = os.path.join(cache_dir, f"{self.source}.dev")
-        if os.path.isfile(devid_patch):
-            with open(devid_patch, 'r') as f:
-                dev_id = f.readline()
-        else:
-            dev_id = uuid.uuid4().int
-            with open(devid_patch, 'w') as f:
-                f.write(f"{dev_id}")
-        return str(dev_id)
 
     def Channels(self):
         LL=[]
@@ -48,7 +53,7 @@ class Scraper:
             try:
                 if not cnLine["source"]["free"]:
                     continue
-                group = self.source
+                group = SOURCE
                 groups = cnLine["categories"]
                 try:
                     for grpLine in groups:
@@ -66,7 +71,7 @@ class Scraper:
 
         if LL:
             # Loading a Tuple into a Database (source, Tuple)
-            utils.ch_inputs_DB(self.source, LL)
+            utils.ch_inputs_DB(SOURCE, LL)
             RET_STATUS = True
 
         return RET_STATUS
