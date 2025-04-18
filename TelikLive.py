291809b392264fa046ac920cf998fc3560942829
AlexELEC <alexelecv5@gmail.com>
2025-04-18 16:01:32 +0300

    TelikLive: update addon

--- TelikLive.py
+++ TelikLive.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+from pathlib import Path
 
 root_dir = os.path.dirname(sys.argv[0])
 libs_dir = os.path.join(root_dir, 'libs')
@@ -18,9 +19,8 @@ from bs4 import BeautifulSoup
 
 class Scraper:
     def __init__(self):
-        self.source = 'TelikLive'
+        self.source = Path(__file__).stem
         self.site = 'http://telik.live'
-        self.link = f'ext:{self.source}:'
         self.headers = {'User-Agent': DEF_BROWSER,
                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                         'Referer': 'http://telik.live/'}
@@ -31,28 +31,46 @@ class Scraper:
     def Channels(self):
         LL=[]
         RET_STATUS = False
+
         http = utils.getURL(self.site, headers=self.headers)
         soup = BeautifulSoup(http, "html.parser")
 
-        for tag in soup.find_all('td', attrs={"style": "text-align: center;"} ):
-            href = None
-            try: href = self.site + tag.find('a').get('href')
-            except: continue
-            if not href: continue
+        # get groups
+        tags = soup.find('ul', {'class': "nav-child unstyled small"})
+        GROUPS = []
 
-            http = utils.getURL(href, headers=self.headers)
-            if not '<iframe allowfullscreen="" name="frame" src="' in http: continue
-            iframe = utils.mfind(http, '<iframe allowfullscreen="" name="frame" src="', '.php"')
-            if not "cdntvmedia.com" in iframe: continue
-
-            url = f"{self.link}{iframe}.php"
+        for tag in tags.find_all('a'):
             try:
-                img = self.site + tag.find('img').get('src')
-                title = tag.find('img').get('title')
-                title = title.replace('прямой эфир', '').strip()
-                ids = utils.title_to_crc32(title)
-                LL.append((ids, title, self.source, url, img))
-            except: pass
+                group = tag.text.strip()
+                group_url = f"{self.site}{tag.get('href')}"
+                GROUPS.append((group, group_url))
+            except:
+                pass
+
+        # get channels
+        for grp in GROUPS:
+            http = utils.getURL(grp[1], headers=self.headers)
+            soup = BeautifulSoup(http, "html.parser")
+
+            for tag in soup.find_all('td', attrs={"style": "text-align: center;"} ):
+                href = None
+                try: href = f"{self.site}{tag.find('a').get('href')}"
+                except: continue
+                if not href: continue
+
+                http = utils.getURL(href, headers=self.headers)
+                if not '<iframe allowfullscreen="" name="frame" src="' in http: continue
+                iframe = utils.mfind(http, '<iframe allowfullscreen="" name="frame" src="', '.php"')
+                if not "cdntvmedia.com" in iframe: continue
+
+                url = f"ext:{self.source}:{iframe}.php"
+                try:
+                    img = f"{self.site}{tag.find('img').get('src')}"
+                    title = tag.find('img').get('title')
+                    title = title.replace('прямой эфир', '').strip()
+                    ids = utils.title_to_crc32(title)
+                    LL.append((ids, title, grp[0], url, img))
+                except: pass
 
         if LL:
             # Loading a Tuple into a Database (source, Tuple)
@@ -64,6 +82,16 @@ class Scraper:
     def getLink(self, lnk):
         http = utils.getURL(lnk, headers=self.headers)
         url = utils.mfind(http, 'id:"player", file:"', '"});')
+
+        if not url.startswith('http'):
+            url = utils.mfind(http, '<iframe src=', '></iframe>')
+            url = url.replace('"', '')
+            parts_url = url.split()
+            for pt in parts_url:
+                if pt.startswith('http'):
+                    url = pt
+                    if '?file=' in url: _, url = pt.split('?file=')
+
         if url.startswith('http'):
             if ' or ' in url:
                 head,sep,tail = url.partition(' or ')
